1ca08240ddeeaf725920cd66903fe9cafdd579a0
João Raimundo <jraimundo21@gmail.com>
2022-04-02 23:21:16 +0100

    Imagens são carregadas do backend; é solicitada classificação

--- src/components/AppHeader/index.js
+++ src/components/AppHeader/index.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
+import layout from '../../styles/responsive-layout';
 
 const styles = {
     main: {
-        backgroundColor: '#394648',
-        color: 'white',
+        backgroundColor: '#365949',
+        color: '#F1F2EB',
     },
 };
 
 export default class AppHeader extends Component {
     render() {
         return (
-            <div className='header-height cui flex row jc-center ai-center' style={styles.main}>
-                <h1>BREAST CANCER IMAGING</h1>
+            <div className={layout.header.grid.head} style={styles.main}>
+                <div className={layout.header.grid.left}></div>
+                <div className={layout.header.grid.center + ' flex as-center'}>
+                    <h1 className='cui text-align-left'>BREAST CANCER IMAGING</h1>
+                </div>
+                <div className={layout.header.grid.right}></div>
             </div>
         );
     }
--- src/containers/DicomStudio/index.js
+++ src/containers/DicomStudio/index.js
@@ -7,9 +7,12 @@ import datasetApi from '../../apis/dataset-api';
 
 // My components
 import URLImage from './URLImage';
-import ToolBox from './ToolBox';
+import CaseInfo from './CaseInfo';
 import MyEllipse from './MyEllipse';
 import Instructions from './Instructions';
+import layout from '../../styles/responsive-layout';
+import SliceControls from './SliceControls';
+import ImageModal from '../../components/ImageModal';
 
 // Initial values
 
@@ -28,9 +31,9 @@ const ellipseDynamicProps = {
     opacity: { still: 0.4, drag: 0.2 },
 };
 
-export default function DicomStudio() {
-    const [caseCodeList, setCaseCodeList] = useState(null);
-    const [mriPassList, setMriPassList] = useState(null);
+export default function DicomStudio({ id }) {
+    const [caseCodes, setCaseCodes] = useState(null);
+    const [mriPasses, setMriPassReferences] = useState(null);
     const [caseCode, setCaseCode] = useState(null);
     const [mriPass, setMriPass] = useState(null);
     const [numberOfSlices, setNumberOfSlices] = useState(null);
@@ -40,69 +43,71 @@ export default function DicomStudio() {
     const [ellipse, setEllipse] = useState(initEllipse);
     const [stageSize, setStageSize] = useState({ width: 480, height: 480 });
     const [hasImageLoaded, setHasImageLoaded] = useState(false);
+    const [clinicalData, setClinicalData] = useState(null);
+    const [classificationResult, setClassificationResult] = useState(null);
+    const [clinicalDataActiveIndex, setClinicalDataActiveIndex] = useState(0);
+    const [waitingForClassification, setWaitingForClassification] = useState(false);
 
     const imageRef = useRef(null);
+    const stageRef = useRef(null);
 
     // Load component data
     useEffect(() => {
-        datasetApi.getCaseCodes().then((caseCodeListResponse) => {
-            setCaseCodeList(caseCodeListResponse.data.data);
+        datasetApi.getCaseCodes().then((caseCodesResponse) => {
+            setCaseCodes(caseCodesResponse.data.data);
         });
     }, [slice]);
 
-    /**
-     * Function to determine wether a shape has been dragged out of stage
-     * @param {object} shape
-     * @returns
-     */
-    const shapeWentOutOfStage = (shape) => {
-        return shape.x < 0 || shape.y < 0 || shape.x > stageSize.width || shape.y > stageSize.height;
-    };
-
     const renderSlice = (num) => {
-        setHasImageLoaded(false);
         datasetApi
             .getSlice(num)
             .then((sliceResponse) => sliceResponse.data)
             .then((data) => {
-                // setStageSize(data.shape);
                 setSlice(data);
                 setSliceNum(num);
+                setHasImageLoaded(true);
             });
     };
 
-    const toolBoxActions = {
-        zoomIn: () => {
-            console.log('zoom-in');
-        },
-        zoomOut: () => {
-            console.log('zoom-out');
-        },
+    const sliceControlActions = {
         prevSlice: () => {
             if (sliceNum <= 1) return;
+            setHasImageLoaded(false);
             renderSlice(sliceNum - 1);
         },
         nextSlice: () => {
             if (sliceNum >= numberOfSlices) return;
+            setHasImageLoaded(false);
             renderSlice(sliceNum + 1);
         },
-        submitSelection: () => {
-            const { x, y, width, height, rotation } = ellipse;
+    };
 
+    const caseInfoActions = {
+        submitSelection: () => {
+            setWaitingForClassification(true);
             datasetApi.sendEllipseData(sliceNum, ellipse).then((result) => {
+                setClassificationResult('GOT SOME RESULT');
+                setClinicalDataActiveIndex(1);
                 console.log(result);
+                setWaitingForClassification(false);
             });
         },
         onCaseCodeDropDownChange: (e, data) => {
+            setClassificationResult(null);
             setCaseCode(data.value);
-            setMriPassList(caseCodeList[data.value]);
+            setSliceNum(null);
+            setMriPassReferences(caseCodes[data.value]);
+            setMriPass(null);
         },
         onMriPassNumDropDownChange: async (e, data) => {
+            setHasImageLoaded(false);
+            setClassificationResult(null);
             setSlice(null);
+            setSliceNum(null);
             setMriPass(data.value);
-            const numSlices = mriPassList[data.value].numslices;
+            const numSlices = mriPasses[data.value].numslices;
             setNumberOfSlices(numSlices);
-            datasetApi.init(mriPassList[data.value].code).then((result) => renderSlice(parseInt(numSlices / 2)));
+            datasetApi.init(mriPasses[data.value].code).then((result) => renderSlice(parseInt(numSlices / 2)));
         },
     };
 
@@ -125,70 +130,105 @@ export default function DicomStudio() {
         onLoad: (e) => {
             const { width, height } = imageRef.current.image;
             setStageSize({ width, height });
-            setHasImageLoaded(true);
         },
+    };
+
+    const triggerModal = (cb, a) => {
+        cb(a);
     };
 
     const styles = {
         stage: {
-            margin: '20px 0 0 0',
             minWidth: `${stageSize.width}px`,
             height: '70vh',
         },
-        imageBox: {
+        stageBox: {
             objectFit: 'fill',
-            backgroundColor: 'white',
-            width: `${stageSize.width + 300}px`,
+            backgroundColor: '#FFFFFB',
+            width: `${stageSize.width}px`,
             height: `${stageSize.height}px`,
         },
-        loader: {
-            // minWidth: `${stageSize.width}px`,
-            // height: '70vh',
+        stageContainer: {
+            backgroundColor: '#535052',
+            // color: 'white',
+            padding: '10px 10px 10px 10px',
+            borderRadius: '5px',
+            // minHeight: '600px',
+            // width: '100%',
+            // backgroundColor: '#CDCBCC',
         },
     };
 
     return (
-        <div className='cui flex column ai-center jc-space-around mt-12'>
-            <div className='cui flex column jc-center'>
-                <ToolBox
-                    actions={toolBoxActions}
-                    caseCodeList={Object.keys(caseCodeList ?? {})}
-                    mriPassList={Object.keys(mriPassList ?? {})}
-                    sliceNum={sliceNum}
+        <div id={id} className={layout.dicomStudio.grid.head}>
+            <div className={layout.dicomStudio.grid.left} style={styles.leftContainer}>
+                <ImageModal
+                    imageUrl={null}
+                    open={waitingForClassification}
+                    setter={triggerModal}
+                    disableControls={true}
+                    title='Classification in progress...'
                 />
-                <div className='cui flex row ai-center jc-space-between' style={styles.imageBox}>
-                    {!mriPass ? (
-                        <Instructions />
-                    ) : !sliceNum || !slice || slice.length < 1 ? (
-                        <div className='cui flex column jc-center ai-center width-100'>
-                            <Loader />
+            </div>
+            <div className={layout.dicomStudio.grid.center + ' flex column jc-center'}>
+                <div className='cui flex row jc-start'>
+                    <div className='cui mr-12'>
+                        <CaseInfo
+                            actions={caseInfoActions}
+                            caseCodes={Object.keys(caseCodes ?? {})}
+                            mriPasses={Object.keys(mriPasses ?? {})}
+                            sliceNum={sliceNum}
+                            clinicalData={clinicalData}
+                            classificationResult={classificationResult}
+                            activeIndex={clinicalDataActiveIndex}
+                            setActiveIndex={setClinicalDataActiveIndex}
+                            disabledMriPassReference={waitingForClassification || !caseCode}
+                            disabledSubmit={waitingForClassification || !hasImageLoaded}
+                            disableCaseSelector={waitingForClassification}
+                        />
+                    </div>
+                    <div className='cui flex column ml-12' style={styles.stageContainer}>
+                        <SliceControls
+                            actions={sliceControlActions}
+                            sliceNum={sliceNum}
+                            width={stageSize.width}
+                            disabled={!hasImageLoaded || waitingForClassification}
+                        />
+                        <div className='cui flex row ai-center' style={styles.stageBox}>
+                            {!mriPass ? (
+                                <Instructions />
+                            ) : !sliceNum || !slice || slice.length < 1 ? (
+                                <div className='cui flex column jc-center ai-center width-100'>
+                                    <Loader />
+                                </div>
+                            ) : (
+                                <div className='cui flex row ac-start'>
+                                    <Stage
+                                        ref={stageRef}
+                                        width={stageSize.width}
+                                        height={stageSize.height}
+                                        onMouseDown={ellipseActions.checkDeselect}
+                                        onTouchStart={ellipseActions.checkDeselect}
+                                    >
+                                        <Layer>
+                                            <URLImage actions={urlImageActions} ref={imageRef} src={datasetApi.baseURL + slice['image_path']} />
+                                            <MyEllipse
+                                                disabled={!hasImageLoaded || waitingForClassification}
+                                                shapeProps={initEllipse}
+                                                dynamicProps={ellipseDynamicProps}
+                                                isSelected={initEllipse.id === selectedId}
+                                                onSelect={ellipseActions.onSelect}
+                                                callbackAttributes={ellipseActions.onChange}
+                                            />
+                                        </Layer>
+                                    </Stage>
+                                </div>
+                            )}
                         </div>
-                    ) : (
-                        <div className='cui flex row ac-start'>
-                            <Stage
-                                width={stageSize.width}
-                                height={stageSize.height}
-                                onMouseDown={ellipseActions.checkDeselect}
-                                onTouchStart={ellipseActions.checkDeselect}
-                            >
-                                <Layer>
-                                    <URLImage actions={urlImageActions} ref={imageRef} src={datasetApi.baseURL + slice['image_path']} />
-                                    <MyEllipse
-                                        shapeProps={initEllipse}
-                                        dynamicProps={ellipseDynamicProps}
-                                        isSelected={initEllipse.id === selectedId}
-                                        onSelect={ellipseActions.onSelect}
-                                        callbackAttributes={ellipseActions.onChange}
-                                    />
-                                </Layer>
-                            </Stage>
-                            <div>
-                                <h1 className='cui mt-12 ml-12'>Clinical Data</h1>
-                            </div>
-                        </div>
-                    )}
+                    </div>
                 </div>
             </div>
+            <div className={layout.dicomStudio.grid.right} style={styles.rightContainer}></div>
         </div>
     );
 }
